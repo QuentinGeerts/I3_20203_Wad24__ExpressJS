dca8c4ace75692a27a30dc0e00eddbb407ee6186
Quentin Geerts <quentin.geerts@bstorm.be>
2025-03-17 15:37:55 +0100

    Exercice 01 - Routing - Énoncé

---
+++ Exercices/index.js
@@ -0,0 +1,22 @@
+/*
+
+Développez une application Node.js avec Express pour gérer des utilisateurs. Implémentez les fonctionnalités suivantes :
+
+Afficher tous les utilisateurs :
+Route GET /utilisateurs pour récupérer la liste de tous les utilisateurs.
+
+Afficher un utilisateur par ID :
+Route GET /utilisateurs/:id pour récupérer un utilisateur spécifique par son ID.
+
+Ajouter un utilisateur :
+Route POST /ajouter-utilisateur pour créer un nouvel utilisateur en envoyant les données (nom, prénom, email) dans le corps de la requête.
+
+Modifier un utilisateur :
+Route PUT /modifier-utilisateur/:id pour mettre à jour un utilisateur spécifique en fonction de son ID.
+
+Supprimer un utilisateur :
+Route DELETE /supprimer-utilisateur/:id pour supprimer un utilisateur en fonction de son ID.
+
+L'application doit gérer les erreurs (utilisateur non trouvé, données manquantes) et répondre avec les statuts HTTP appropriés.
+
+*/
